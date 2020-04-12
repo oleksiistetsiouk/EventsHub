52f9c0d4da62fe1e5b5c67e8da6b28ecd861d0d7
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-04-12 17:31:23 +0300

    add mobile client services

---
+++ src/mobile/EventsHub.Mobile/Services/ServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Xamarin.Essentials;
+
+namespace EventsHub.Mobile.Services
+{
+    public class ServiceBase
+    {
+        protected HttpClient httpClient;
+        protected bool IsConnected => Connectivity.NetworkAccess == NetworkAccess.Internet;
+
+        public ServiceBase()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri($"{App.BackendUrl}/");
+        }
+    }
+}
---
+++ src/mobile/EventsHub.Mobile/Services/TheatreService.cs
@@ -0,0 +1,37 @@
+﻿using EventsHub.Mobile.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EventsHub.Mobile.Services
+{
+    public class TheatreService : ServiceBase
+    {
+        public async Task<TheatrePlay> GetTheatrePlay(int id)
+        {
+            if (IsConnected)
+            {
+                TheatrePlay play = null;
+                var json = await httpClient.GetStringAsync($"api/theatre/{id}");
+                play = await Task.Run(() => JsonConvert.DeserializeObject<TheatrePlay>(json));
+
+                return play;
+            }
+            return null;
+        }
+
+        public async Task<IEnumerable<TheatrePlay>> GetAllTheatrePlays()
+        {
+            if (IsConnected)
+            {
+                IEnumerable<TheatrePlay> plays = null;
+                var json = await httpClient.GetStringAsync($"api/theatre");
+                plays = await Task.Run(() => JsonConvert.DeserializeObject<IEnumerable<TheatrePlay>>(json));
+
+                return plays;
+            }
+            return null;
+        }
+    }
+}
