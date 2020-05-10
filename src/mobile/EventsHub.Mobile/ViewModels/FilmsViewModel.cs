e45fb2b131d0d1907e7b92aeb999a9174a2a36d6
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-05-10 19:57:37 +0300

    page detail styles

--- src/mobile/EventsHub.Mobile/ViewModels/FilmsViewModel.cs
+++ src/mobile/EventsHub.Mobile/ViewModels/FilmsViewModel.cs
@@ -37,7 +37,7 @@ namespace EventsHub.Mobile.ViewModels
 
         public FilmsViewModel()
         {
-            Title = "Films";
+            Title = "Фільми";
             FilmTreshold = 4;
             pageNumber = 1;
             filmService = new FilmService();
