fe390193afb995c177425db58a13164194a063e7
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-05-16 19:49:20 +0300

    Cleanup mobile services

--- src/mobile/EventsHub.Mobile/Services/Client/FilmService.cs
+++ src/mobile/EventsHub.Mobile/Services/Client/FilmService.cs
@@ -11,19 +11,6 @@ namespace EventsHub.Mobile.Services.Client
 {
     class FilmService : ServiceBase
     {
-        public async Task<Film> GetFilm(int id)
-        {
-            if (IsConnected)
-            {
-                Film film = null;
-                var json = await httpClient.GetStringAsync(string.Format($"{Api.GetFilm}", id));
-                film = await Task.Run(() => JsonConvert.DeserializeObject<Film>(json));
-
-                return film;
-            }
-            return null;
-        }
-
         public async Task<IEnumerable<Film>> GetAllFilms(int pageNumber = 1)
         {
             var url = string.Format($"{Api.GetFilms}", pageNumber, AppConstants.PageSize);
--- src/mobile/EventsHub.Mobile/Services/Client/TheatreService.cs
+++ src/mobile/EventsHub.Mobile/Services/Client/TheatreService.cs
@@ -8,19 +8,6 @@ namespace EventsHub.Mobile.Services.Client
 {
     public class TheatreService : ServiceBase
     {
-        public async Task<TheatrePlay> GetTheatrePlay(int id)
-        {
-            if (IsConnected)
-            {
-                TheatrePlay play = null;
-                var json = await httpClient.GetStringAsync($"api/theatre/{id}");
-                play = await Task.Run(() => JsonConvert.DeserializeObject<TheatrePlay>(json));
-
-                return play;
-            }
-            return null;
-        }
-
         public async Task<IEnumerable<TheatrePlay>> GetAllTheatrePlays()
         {
             if (IsConnected)
