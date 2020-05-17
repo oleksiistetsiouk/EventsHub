77a351d59bd5629f5e8d8ac29c4350e340be871d
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-05-17 14:35:20 +0300

    update TheatrePlay models

--- src/mobile/EventsHub.Mobile/Models/TheatrePlay.cs
+++ src/mobile/EventsHub.Mobile/Models/TheatrePlay.cs
@@ -7,5 +7,8 @@
         public string Place { get; set; }
         public int PriceFrom { get; set; }
         public int PriceTo { get; set; }
+        public DateTime Date { get; set; }
+        public string PosterUrl { get; set; }
+        public string DirectLink { get; set; }
     }
 }
